cefda3783745bd3243d5b00a89005f59cde87495
IJ McC <IJ McC>
2017-01-17 22:08:32 -0500

    CORS - now they talk to each other :D

--- api-server.js
+++ api-server.js
@@ -12,6 +12,12 @@ var port       = process.env.PORT || 3030;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var neo4j = require('neo4j');
 var db = new neo4j.GraphDatabase('http://neo4j:n@localhost:7474');
