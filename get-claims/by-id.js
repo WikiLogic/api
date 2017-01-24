ee010abf49b2d0290b119440a6c51120c12a01cf
IJ McC <IJ McC>
2017-01-24 07:29:11 -0500

    tidy the data coming through - need to pull out from and to ids, looks like it might need some kind of cypher function

--- get-claims/by-id.js
+++ get-claims/by-id.js
@@ -8,30 +8,18 @@ var db = require('../neo4j/neo-connection.js');
  *  - the claims that make up those arguments 
  */
 
-var exampleResponce = {
-    claim: 'claim object',
-    arguments: [ 'argument objects' ],
-    claims: [ 'sub claim objects' ],
-    links: [ 'link objects' ]
-
-}
-
-var builder = `MATCH (claim:Claim)
-                    WHERE ID(claim) = 4 
-                    WITH claim
-                    OPTIONAL MATCH immidiatePath = (subClaim:Claim)-[subLink]->(arguments:ArgGroup)-[focusLink]->(claim)
-                    WITH claim, nodes(immidiatePath) as nodes, relationships(immidiatePath) as links
-                    UNWIND nodes as nodelist
-                    UNWIND links as linklist
-                    RETURN claim, nodelist, linklist
-                    LIMIT 25`;
 module.exports = function(req, res){
 
     try {
         db.cypher({
-            query: `OPTIONAL MATCH p = ((subClaim:Claim)-[supLinks]->(arguments:ArgGroup)-[argLinks]->(claim:Claim))
+            query: `OPTIONAL MATCH p = ((subClaim:Claim)-[subLink]->(argument:ArgGroup)-[argLink]->(claim:Claim))
                     WHERE ID(claim) = ${req.params.claimid} 
-                    RETURN claim, COLLECT(subClaim) AS subClaims, COLLECT(arguments) as arguments, COLLECT(argLinks) as argLinks, COLLECT(supLinks) as supLinks
+                    RETURN  
+                        {id: id(claim), body: claim.body, state: claim.state} AS claim, 
+                        COLLECT({id: ID(subClaim), body: subClaim.body, state: subClaim.state}) AS subClaims, 
+                        COLLECT({id: ID(argument), state: argument.state}) as argument, 
+                        COLLECT(argLink) as argLinks, 
+                        COLLECT(subLink) as subLinks
                     LIMIT 100`
         }, function (err, results) {
             if (err) throw err;
@@ -41,32 +29,22 @@ module.exports = function(req, res){
                 res.json({ error: 'No claims found' });
             } else {
 
-                //================ This is the happy path
-                var claim = {};
-                var argumentGroups = [];
-                var claims = [];
-                var links = [];
-                console.log("happy path!",results);
+                if (results.length == 0){
+                    res.json({
+                        meta: 'No claims match ' + req.params.claimid,
+                        data: {}
+                    });    
+                } 
 
-                // if (results.length > 0){
-                //     results.map(function(match) {
-                        
-                //         claims.push({
-                //             id: match.claim._id,
-                //             type: 'claim',
-                //             body: match.claim.properties.body,
-                //             state: match.claim.properties.state,
-                //         });
-                //     })
-                // }
+                if (results.length > 1){
+                    //should only return one claim when getting by id... something's wrong with the data (scream!)
 
-                res.json(results);
-                // res.json({
-                //     meta: 'aint no meta here yet',
-                //     data: {
-                //         claim: claims
-                //     }
-                // });
+                }
+
+                res.json({
+                    meta: 'No meta yet',
+                    data: results[0]
+                });
             }
         });
 
