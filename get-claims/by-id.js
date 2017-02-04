b7887dfc44edc76560fd29e927b3fd6985e67615
ijmccallum <ijmccallum@hotmail.co.uk>
2017-02-04 14:03:00 -0500

    comment out used in by default, will split this into it's own thing

--- get-claims/by-id.js
+++ get-claims/by-id.js
@@ -11,15 +11,16 @@ var db = require('../neo4j/neo-connection.js');
 
 module.exports = function(req, res){
 
+                    //this would return USED_IN links and thir argument nodes - but not the other claims that make up uthose argument groups
+                    //OPTIONAL MATCH (claim)-[usedInLink]->(usedInArg)
+                    // CASE WHEN ID(usedInArg) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(usedInArg), state: usedInArg.state, type: "argument"}) END AS usedInArgs,
+                    // CASE WHEN ID(usedInArg) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(usedInLink), type: "USED_IN", source: ID(startNode(usedInLink)), target: ID(endNode(usedInLink))}) END AS usedInLinks,
     try {
         db.cypher({
             query: `MATCH (claim:Claim)
                     WHERE ID(claim) = ${req.params.claimid} 
                     OPTIONAL MATCH (subClaim:Claim)-[subLink]->(argument:ArgGroup)-[argLink]->(claim)
-                    OPTIONAL MATCH (claim)-[usedInLink]->(usedInArg)
                     RETURN  
-                        CASE WHEN ID(usedInArg) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(usedInArg), state: usedInArg.state, type: "argument"}) END AS usedInArgs,
-                        CASE WHEN ID(usedInArg) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(usedInLink), type: "USED_IN", source: ID(startNode(usedInLink)), target: ID(endNode(usedInLink))}) END AS usedInLinks,
                         {id: id(claim), body: claim.body, state: claim.state, type: "claim"} AS claim,
                         CASE WHEN ID(argument) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(argLink), type: TYPE(argLink), source: ID(startNode(argLink)), target: ID(endNode(argLink))}) END AS argLinks, 
                         CASE WHEN ID(argument) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(argument), state: argument.state, type: "argument"}) END AS arguments, 
