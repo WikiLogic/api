4f7fc8a02c0446ef9c5e2605c80fcfa18f55e784
Douglas <douglas@sophiesgames.com>
2017-03-27 12:04:32 +0100

    query that gets an id's arg groups and the claims within those arg groups

--- get-claims/by-id.js
+++ get-claims/by-id.js
@@ -14,20 +14,11 @@ module.exports = function(req, res){
                     //this would return USED_IN links and thir argument nodes - but not the other claims that make up uthose argument groups
     try {
         db.cypher({
-            query: `MATCH (claim:Claim)
-                    WHERE ID(claim) = ${req.params.claimid} 
-                    OPTIONAL MATCH (subClaim:Claim)-[subLink]->(argument:ArgGroup)-[argLink]->(claim)
-                    OPTIONAL MATCH (claim)-[usedInLink]->(usedInArg)<-[usedInSiblingLink]-(usedInSibling)
-                    RETURN  
-                        CASE WHEN ID(usedInArg) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(usedInSibling), body: usedInSibling.body, state: usedInSibling.state, type: "claim"}) END AS usedInSiblings,
-                        CASE WHEN ID(usedInArg) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(usedInSiblingLink), type: "USED_IN", source: ID(startNode(usedInSiblingLink)), target: ID(endNode(usedInLink))}) END AS usedInSiblingLinks,
-                        CASE WHEN ID(usedInArg) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(usedInArg), state: usedInArg.state, type: "argument"}) END AS usedInArgs,
-                        CASE WHEN ID(usedInArg) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(usedInLink), type: "USED_IN", source: ID(startNode(usedInLink)), target: ID(endNode(usedInLink))}) END AS usedInLinks,
-                        {id: id(claim), body: claim.body, state: claim.state, type: "claim"} AS claim,
-                        CASE WHEN ID(argument) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(argLink), type: TYPE(argLink), source: ID(startNode(argLink)), target: ID(endNode(argLink))}) END AS argLinks, 
-                        CASE WHEN ID(argument) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(argument), state: argument.state, type: "argument"}) END AS arguments, 
-                        CASE WHEN ID(argument) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(subLink), type: TYPE(subLink), source: ID(startNode(subLink)), target: ID(endNode(subLink))}) END AS subLinks,
-                        CASE WHEN ID(argument) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(subClaim), body: subClaim.body, state: subClaim.state, type: "claim"}) END AS subClaims
+            query: `match (callingClaim:Claim)<-[:UsedFor]-(c:ArgGroup)<-[:UsedIn]-(n:Claim)
+                    WHERE ID(callingClaim) = ${req.params.claimid} 
+                    with c, collect({ id: id(n), body: n.body, type: labels(n)[0] }) as claims
+                    with { id: id(c), type: labels(c)[0], SubClaims: claims } as argument
+                    return {claims: collect(argument) }
                     LIMIT 100`
         }, function (err, results) {
             if (err) throw err;
