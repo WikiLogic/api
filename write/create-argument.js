e353887973b0df9f5618a0a9b86862ddb5e8854a
ijmccallum <ijmccallum@hotmail.co.uk>
2017-04-04 19:39:19 -0400

    setting up new argument route

--- write/create-argument.js
+++ write/create-argument.js
@@ -1,12 +1,14 @@
 "use strict";
 var db = require('../neo4j/neo-connection.js');
 
-/* /claims/:claimid
- * returns:
+/* /create/argument POST data expects:
+ *      parent_claim: ID
+ *      type: "SUPPORTS" / "OPPOSES"
+ *      premises: [ID, ID, ID] array minimum of 2?
  * 
- *  - the claim who's ID has been passed
- *  - the arguments that link to it
- *  - the claims that make up those arguments 
+ * returns:
+ *  - 6107336639
+ *  - The parent claim filled out with all it's arguments 
  */
 
 var returnObj = {
@@ -15,40 +17,41 @@ var returnObj = {
         "id": "33",
         "text": "text text",
         "state": "null",
-        "arguments": [
-            {
-                "type": "SUPPORTS",
-                "state": "50",
-                "premises": [
-                    {
-                        "id": "34",
-                        "text": "premis text",
-                        "state": "50"
-                    }
-                ]
-            }
-        ]
+        "arguments": []
     }
 }
 
-module.exports = function(req, res){
+function testPostData(postData){
+    if (!postData.hasOwnProperty('parent_claim')) {
+        return false;
+    }
+    if (!postData.hasOwnProperty('type') || (postData.type != 'SUPPORTS' || postData.type != 'OPPOSES') ) {
+        return false;
+    }
+    if (!postData.hasOwnProperty('premises') || postData.premises.length < 2) {
+        return false;
+    }
+    return true;
+}
 
-                    //this would return USED_IN links and thir argument nodes - but not the other claims that make up uthose argument groups
-                        //CASE WHEN ID(argument) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(subLink), type: TYPE(subLink), source: ID(startNode(subLink)), target: ID(endNode(subLink))}) END AS subLinks,
-                        //CASE WHEN ID(argument) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(argLink), type: TYPE(argLink), source: ID(startNode(argLink)), target: ID(endNode(argLink))}) END AS argLinks, 
+module.exports = function(req, res){
+    
+    console.log("TODO: escape post data");
+
+    if (!testPostData(req.body)) {
+        return res.json({
+            errors: 'malformed post data',
+        });
+    }
+
+    var premisMatch = req.body.premises;
+    premisMatch.toString();
+
     try {
         db.cypher({
-            query: `MATCH (claim)
-                    WHERE (claim:Claim OR claim:Axiom) AND (ID(claim) = ${req.params.claimid})
-                    OPTIONAL MATCH (argument:ArgGroup)-[argLink]->(claim)
-                    OPTIONAL MATCH (premis:Claim)-[premisLink]->(argument)
-                    WITH claim, argument, argLink, 
-                        CASE WHEN ID(premis) IS NULL THEN null ELSE {id: ID(premis), text: premis.text, labels: LABELS(premis), state: premis.state} END AS premises
-                    WITH claim, 
-                        CASE WHEN ID(argument) IS NULL THEN null ELSE {id: ID(argument), type:TYPE(argLink), state: argument.state, premises: COLLECT(premises)} END AS arguments 
-                    WITH {id: id(claim), text: claim.text, labels: LABELS(claim), state: claim.state, arguments: COLLECT(arguments)} AS claim
-                    RETURN claim
-                    LIMIT 100`
+            query: `MATCH (claim:Claim), (premis:Claim)
+                    WHERE ID(claim) = ${req.body.parent_claim} AND ID(premis) IN [${premisMatch}]
+                    RETURN claim premis`
         }, function (err, results) {
             if (err) throw err;
             
@@ -59,20 +62,15 @@ module.exports = function(req, res){
                 if (results.length == 0){
                     
                     res.json({
-                        meta: 'No claims match ' + req.params.claimid,
+                        meta: 'Claim was not returned, probably means it wasn\'t created ',
                         data: {}
                     });  
                     return;  
                 } 
 
-                if (results.length > 1){
-                    //should only return one claim when getting by id... something's wrong with the data (scream!)
-                    console.log('many many many');
-                }
-
                 res.json({
                     meta: 'No meta yet',
-                    data: results[0]
+                    data: results
                 });
             }
         });
