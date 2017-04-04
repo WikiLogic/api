146bf4ceb2d9e1aedd61f1939cb370019c10ff89
Iain McCallum <imccallum@delphicdigital.com>
2017-04-04 08:28:46 -0400

    setting up create routes

--- api-server.js
+++ api-server.js
@@ -24,23 +24,40 @@ var db = require('./neo4j/neo-connection.js');
 var getClaims = require('./get-claims/_index.js');
 
 //================================= Routes
+
 var apiRouter = express.Router();
 
+//--reading
 apiRouter.get('/claims', function(req, res){
     if (req.query.hasOwnProperty('search')){
         getClaims.bySearchTerm(req, res);
     }
 });
-
 apiRouter.get('/claims/random', function(req, res){
     getClaims.random(req, res);
 });
-
 apiRouter.get('/claims/:claimid', function(req, res){
     getClaims.byId(req, res);
 });
 
+//--writing
+apiRouter.post('/create/claim', function(req, res, next){
+    console.log("create claim - check authentication");
+    next();
+}, function(req, res){
+    console.log("create claim");
+    res.json({api: 'says hello!'});
+});
+apiRouter.post('/create/argument', function(req, res, next){
+    console.log("create claim - check authentication");
+    next();
+}, function(req, res){
+    console.log("create argument");
+    res.json({api: 'says hello!'});
+});
+
 app.use('/', apiRouter);
+
 
 
 
