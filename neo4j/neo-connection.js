1371368d0517fbdb8631876ddb8c6c96dc973efa
ijmccallum <ijmccallum@hotmail.co.uk>
2017-05-31 22:04:10 -0400

    working towards connecting containers

--- neo4j/neo-connection.js
+++ neo4j/neo-connection.js
@@ -1,8 +1,15 @@
 "use strict";
 
-var neo4j = require('neo4j');
-var db = new neo4j.GraphDatabase('http://neo4j:neo4j@localhost:7474');
+//https://github.com/aseemk/node-neo4j-template/blob/master/models/user.js
+//looks like a good example of how to use thie neo4j package in a better way :)
 
+var neo4j = require('neo4j');
+
+var db = new neo4j.GraphDatabase({
+    url: process.env.NEO4J_URL || 'http://neo4j:neo4j@localhost:7474'
+});
+
+console.log('db', db);
 /**
  * An error handler for when the db failes in some way,
  * Log the fail
