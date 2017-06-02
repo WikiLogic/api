88bb7534319731b3ff10885967a83f693d2fd571
ijmccallum <ijmccallum@hotmail.co.uk>
2017-06-01 21:36:06 -0400

    return after err to avoide double headers

--- read/by-id.js
+++ read/by-id.js
@@ -52,6 +52,7 @@ module.exports = function(req, res){
                         //CASE WHEN ID(argument) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(subLink), type: TYPE(subLink), source: ID(startNode(subLink)), target: ID(endNode(subLink))}) END AS subLinks,
                         //CASE WHEN ID(argument) IS NULL THEN [] ELSE COLLECT(DISTINCT {id: ID(argLink), type: TYPE(argLink), source: ID(startNode(argLink)), target: ID(endNode(argLink))}) END AS argLinks, 
     try {
+        console.log('trying connection');
         neo.db.cypher({
             query: `MATCH (claim)
                     WHERE (claim:Claim OR claim:Axiom) AND (ID(claim) = ${req.params.claimid})
@@ -71,6 +72,7 @@ module.exports = function(req, res){
                     meta: 'There was a server error, :/',
                     data: {}
                 });
+                return;
             }
             
             if (!results) {
