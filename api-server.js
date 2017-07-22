c46da883de7393b46fb78c247dbc47ac1aa4c4c4
ijmccallum <ijmccallum@hotmail.co.uk>
2017-07-21 21:00:39 -0400

    jwt auth!

--- api-server.js
+++ api-server.js
@@ -2,18 +2,50 @@
  * This will deal with handling the distrobution of requests to the static file server or api
  */
 
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var path       = require('path');
+var _ = require("lodash");
+var express = require('express'); // call express
 var bodyParser = require('body-parser');
-var port       = process.env.PORT || 3030;
-var morgan     = require('morgan');
+var port = process.env.PORT || 3030;
+var morgan = require('morgan');
 
+var passport = require("passport"); // authentication!
+var jwt = require('jsonwebtoken');
+var passportJWT = require("passport-jwt");
+
+var app = express(); // define our app using express
+
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+
+var users = [
+  {
+    id: 1,
+    name: 'demo',
+    password: 'demo'
+  }
+];
+
+var jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeader();
+jwtOptions.secretOrKey = 'tasmanianDevil';
+
+var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+  // usually this would be a database call:
+  var user = users[_.findIndex(users, {id: jwt_payload.id})];
+  if (user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+});
+
+passport.use(strategy);
+app.use(passport.initialize());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('combined'));
- 
- 
+
 //CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -34,74 +66,93 @@ app.set('trust proxy', function (ip) {
 
 
 var neo = require('./neo4j/neo-connection.js');
-
-
 var getClaims = require('./read/_index.js');
 var getArgs = require('./read/_index.js');
 var create = require('./write/_index.js');
 
 
-//================================= Routes
+app.post("/login", function(req, res) {
+  if(req.body.name && req.body.password){
+    var name = req.body.name;
+    var password = req.body.password;
+  }
+  // usually this would be a database call:
+  var user = users[_.findIndex(users, {name: name})];
+  if( ! user ){
+    res.status(401).json({message:"no such user found"});
+  }
+
+  if(user.password === req.body.password) {
+    // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
+    var payload = {id: user.id};
+    var token = jwt.sign(payload, jwtOptions.secretOrKey);
+    res.json({message: "ok", token: token});
+  } else {
+    res.status(401).json({message:"passwords did not match"});
+  }
+});
+
+
+//================================= API Routes
 
 var apiRouter = express.Router();
 
-//--reading
-apiRouter.get('/', function(req, res){
-    res.send('WL API');
-});
-apiRouter.get('/claims', function(req, res){
-    if (req.query.hasOwnProperty('search')){
-        getClaims.bySearchTerm(req, res);
-    }
-});
-apiRouter.get('/claims/random', function(req, res){
-    getClaims.random(req, res);
-});
-apiRouter.get('/claims/:claimid', function(req, res){
-    getClaims.byId(req, res);
-});
-apiRouter.get('/args/:claimid', function (req, res) {
-    getArgs.byClaimId(req, res);
-});
-
-//--writing
-apiRouter.post('/create/claim', function(req, res, next){
-    console.log("TODO: check authentication");
-    next();
-}, function(req, res){
-    create.claim(req, res);
-});
-
-apiRouter.post('/create/argument', function(req, res, next){
-    console.log("TODO: check authentication");
-    next();
-}, function(req, res){
-    create.argument(req, res);
-});
-
-apiRouter.post('/create/explanation', function (req, res, next) {
-    console.log("TODO: check authentication");
-    next();
-}, function (req, res) {
-    create.explanation(req, res);
-});
-
-//--development
-apiRouter.get('/test', function(req, res){
-    console.log('test route');
-    neo.db.cypher({
-        query: "CALL dbms.procedures()"
-    }, function (err, results) {
-        res.json({err:err,results:results});
+    //--reading
+    apiRouter.get('/', function(req, res){
+        res.send('WL API');
     });
-});
+    apiRouter.get('/claims', function(req, res){
+        if (req.query.hasOwnProperty('search')){
+            getClaims.bySearchTerm(req, res);
+        }
+    });
+    apiRouter.get('/claims/random', function(req, res){
+        getClaims.random(req, res);
+    });
+    apiRouter.get('/claims/:claimid', function(req, res){
+        getClaims.byId(req, res);
+    });
+    apiRouter.get('/args/:claimid', function (req, res) {
+        getArgs.byClaimId(req, res);
+    });
 
-app.use('/api', apiRouter);
+    //--writing
+    apiRouter.post('/create/claim', function(req, res, next){
+        console.log("TODO: check authentication");
+        next();
+    }, function(req, res){
+        create.claim(req, res);
+    });
+
+    apiRouter.post('/create/argument', function(req, res, next){
+        console.log("TODO: check authentication");
+        next();
+    }, function(req, res){
+        create.argument(req, res);
+    });
+
+    apiRouter.post('/create/explanation', function (req, res, next) {
+        console.log("TODO: check authentication");
+        next();
+    }, function (req, res) {
+        create.explanation(req, res);
+    });
+
+    //--development
+    apiRouter.get('/test', function(req, res){
+        console.log('test route');
+        neo.db.cypher({
+            query: "CALL dbms.procedures()"
+        }, function (err, results) {
+            res.json({err:err,results:results});
+        });
+    });
+
+app.use('/api', passport.authenticate('jwt', { session: false }), apiRouter);
 
 app.get('/', function (req, res) {
   res.send('API rooter tooter');
-})
-
+});
 
 //================================= Begin
 app.listen(port);
