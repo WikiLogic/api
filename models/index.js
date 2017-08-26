b228e3c28791f10abade2e9e32ad07a71538681c
ijmccallum <ijmccallum@hotmail.co.uk>
2017-08-26 08:32:58 -0400

    starting to restructure

--- models/index.js
+++
@@ -1,5 +0,0 @@
-Database = require('arangojs').Database;
-
-db = new Database(process.env.ARANGO_URL || 'http://arango:8529');
-
-console.log("db", db);
---
+++ src/_arango/_db.js
@@ -0,0 +1,45 @@
+Database = require('arangojs').Database;
+db = new Database(process.env.ARANGO_URL || 'http://arango:8529');
+
+var database_name = "wl_dev";
+var usersCollection, nodesCollection;
+function initDbConnection(){
+    db.listDatabases()
+    .then((names) => {
+        if (names.indexOf(database_name) > -1){
+            db.useDatabase(database_name);
+            initDbCollections();
+            db.get().then(
+                ()=> console.log("Using database "+database_name),
+                error=> console.error("Error connecting to database: "+error)
+            );
+        } else {
+            db.createDatabase(database_name).then(
+                () => {
+                    console.log("Database created successfully: "+database_name);
+                    db.useDatabase(database_name);
+                    initDbCollections();
+                },
+                error=> console.error("Error creating database: "+error)
+            );
+        }
+    });
+}
+
+function initDbCollections(){
+    usersCollection = db.collection('users');
+    nodesCollection = db.collection('nodes');
+}
+
+function get(collection){
+
+}
+
+function set(collection, id){
+
+}
+
+module.exports = {
+    get,
+    set
+}
